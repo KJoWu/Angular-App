6a5cf4db764e2c94d11635eb02ec7923853d4e60
Silver Rook <silverrook@uws089066usr.uws.ualberta.ca>
2015-10-13 15:40:58 -0600

    linked javascript product

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-var app = angular.module("store", [ ]);
+// Code goes here
+
+(function() {
+  var app = angular.module('gemStore', [ ]);
+
+  app.controller('StoreController', function() {
+    this.product = gem;
+  });
+
+  var gem = {
+  	name:"asdasd",
+  	price: 2.95,
+  	descriptiom: '..'
+  }
+
+
+})();
