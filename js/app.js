954c2da5b3d23c29da11614be5e6a6a36c96743a
Silver Rook <silverrook@Silvers-MBP.ed.shawcable.net>
2015-10-13 22:47:27 -0600

    integrating compass

--- js/app.js
+++ js/app.js
@@ -1,27 +1,64 @@
 // Code goes here
 
 (function() {
-  var app = angular.module('gemStore', [ ]);
+    var app = angular.module('gemStore', []);
 
-  app.controller('StoreController', function() {
-    this.products = gems;
-  });
+    app.controller('StoreController', function() {
+        this.products = gems;
+    });
 
-  var gems = [
-  {
-  	name:"Cool Product 1",
-  	price: 2.95,
-  	description: "it's cool thing",
-  	canPurchase:true,
-  	soldOut:false
-  },
-  {
-  	name:"Cool Product 1",
-  	price: 2.95,
-  	description: "it's cool thing",
-  	canPurchase:true,
-  	soldOut:false
-  }
-  ];
+    var gems = [{
+        name: "Cool Product 1",
+        price: 2,
+        description: "it's cool thing",
+        canPurchase: true,
+        soldOut: false,
+        image: 'img/1.jpg'
+    }, {
+        name: "Cool Product 1",
+        price: 2,
+        description: "it's cool thing",
+        canPurchase: true,
+        soldOut: false,
+        image: 'img/2.jpg'
+    }, {
+        name: "Cool Product 1",
+        price: 2,
+        description: "it's cool thing",
+        canPurchase: true,
+        soldOut: false,
+        image: 'img/3.jpg'
+    }, {
+        name: "Cool Product 1",
+        price: 2,
+        description: "it's cool thing",
+        canPurchase: true,
+        soldOut: false,
+        image: 'img/4.jpg'
+    }, {
+        name: "Cool Product 1",
+        price: 2,
+        description: "it's cool thing",
+        canPurchase: true,
+        soldOut: false,
+        image: 'img/5.jpg'
+    },
+    {
+        name: "Cool Product 1",
+        price: 2,
+        description: "it's cool thing",
+        canPurchase: true,
+        soldOut: false,
+        image: 'img/6.jpg'
+    },
+    {
+        name: "Cool Product 1",
+        price: 2,
+        description: "it's cool thing",
+        canPurchase: true,
+        soldOut: false,
+        image: 'img/7.jpg'
+    }
+    ];
 
 })();
